7a3617845306a6c62a95f42eb3370ca994400b03
sunny123612 <122340172+sunny123612@users.noreply.github.com>
2023-01-11 11:22:10 +0530

    adding

---
+++ Test Suites/testsuite/ts1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c0e65345-5cf5-4191-8e6f-aa3e7d8a291e</testSuiteGuid>
+   <testCaseLink>
+      <guid>3bfcf2b0-1264-445a-ad83-35c1862a83f4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testc1/testcase1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
